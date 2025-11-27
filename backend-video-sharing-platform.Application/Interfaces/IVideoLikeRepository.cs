080b57edc046e77e4db5bcf5142ff9bda9e838d9
WDev9911 <longdtse184444@fpt.edu.vn>
2025-11-27 10:57:33 +0700

    Like and Unlike API

---
+++ backend-video-sharing-platform.Application/Interfaces/IVideoLikeRepository.cs
@@ -0,0 +1,13 @@
+﻿using backend_video_sharing_platform.Domain.Entities;
+
+namespace backend_video_sharing_platform.Application.Interfaces
+{
+    public interface IVideoLikeRepository
+    {
+        Task<VideoLike?> GetAsync(string userId, string videoId);
+        Task SaveAsync(VideoLike like);
+        Task DeleteAsync(string userId, string videoId);
+        Task<List<VideoLike>> GetUserLikesAsync(string userId);
+        Task<int> CountVideoLikesAsync(string videoId);
+    }
+}
